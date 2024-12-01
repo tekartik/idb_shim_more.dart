485a9715a14b3ff0f8d96da2c85d4549db4f7bad
Alexandre Roux <alex@tekartik.com>
2024-12-01 18:28:07 +0100

    fix cond import on the web

---
+++ packages/sembast_web_html_compat/test/compat_test.dart
@@ -0,0 +1,12 @@
+import 'package:idb_shim_html_compat/idb_client_native_html.dart';
+import 'package:test/test.dart';
+
+Future<void> main() async {
+  test('compile on io', () {
+    try {
+      idbFactoryNativeHtml;
+    } catch (e) {
+      print(e);
+    }
+  });
+}
